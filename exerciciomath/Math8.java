d948a8001d6ec28dca08d635635b0045fa19e0f6
Willian Matheus <109594242+willian-matheus@users.noreply.github.com>
2023-08-28 19:35:19 -0300

    Update Math8.java

--- exerciciomath/Math8.java
+++ exerciciomath/Math8.java
@@ -1,13 +1,28 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package exerciciomath;
 
-/**
- *
- * @author willian_tesche
- */
+import javax.swing.JOptionPane;
+
 public class Math8 {
-    
+    public static void main(String[] args) {
+        int numero, cont=1;
+        double quadrado, soma = 0;
+        
+        do{
+            numero = Integer.parseInt(JOptionPane.showInputDialog("Informe o numero maior do que 0 e o menor que 10"));
+        }while (numero <= 0 || numero >= 10);
+        
+        
+        do{
+            if(numero % 2 !=0){
+                quadrado = Math.pow(numero,2);
+                soma += quadrado;
+                cont++;
+                
+                System.out.println(cont + " o valor " + numero + " elevador ao quadrado " + quadrado + " soma dos quadrados " +soma);
+            }
+            numero++;
+        }while(cont < 20);
+        
+    }
 }
